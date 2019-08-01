ec6589cf4f7d6e97d8344d6046de85a295053dc8
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-02 01:18:36 +0800

    add git hash

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,15 @@ use argparse::{ArgumentParser, StoreTrue, Store};
 use rust_util::*;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+const GIT_HASH: &str = env!("GIT_HASH");
 
 fn print_version() {
-    print!(r#"finding {}
+    print!(r#"finding {} - {}
 Copyright (C) 2019 Hatter Jiang.
 License MIT <https://opensource.org/licenses/MIT>
 
 Written by Hatter Jiang
-"#, VERSION);
+"#, VERSION, &GIT_HASH[0..7]);
 }
 
 fn get_term_width() -> Option<usize> {
